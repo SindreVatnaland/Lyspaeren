e4fe7a1982d8335b826ab825bd47e9ae3c9cc9a9
Sindre Vatnaland <69798990+SindreVatnaland@users.noreply.github.com>
2021-10-21 09:59:37 +0200

    render card dynamically

--- components/MainPage/MainPage.tsx
+++ components/MainPage/MainPage.tsx
@@ -4,6 +4,7 @@ import { StyleSheet, TouchableOpacity } from 'react-native';
 import { Card } from 'react-native-paper';
 
 import Colors from '../../constants/Colors';
+import { cardData } from '../cardData';
 import { MonoText } from '../StyledText';
 import { Text, View } from '../Themed';
 import CardItem from './CardItem';
@@ -11,42 +12,14 @@ import CardItem from './CardItem';
 export default function MainPage({ path }: { path: string }) {
   return (
     <View>
+        {cardData().map(( card, index ) => (
         <CardItem
-          CardTitle="Fiks mobilen min"
-          CardDescription="Ny iPhone med skader på skjermen"
-          CardRating="★4.3"
+          key = {index}
+          CardTitle={card.cardTitle}
+          CardDescription={card.cardDescription}
+          CardRating={card.cardRating}
          />
-        <CardItem 
-          CardTitle="Bilradio å bil å sånn spennande"
-          CardDescription="Problemer med å få inn signaler"
-          CardRating="★3.7"
-        />
-        <CardItem 
-          CardTitle="iPhone skjerm"
-          CardDescription="Ødelagt skjerm"
-          CardRating="★4.8"
-        />
-        <CardItem 
-          CardTitle="Brødrister"
-          CardDescription="Vannskader"
-          CardRating="★3.6"
-        />
-        <CardItem 
-          CardTitle="Ps4 Overoppheter"
-          CardRating="★1.7"/>
-        <CardItem 
-          CardTitle="iPhone 5"
-          CardRating="★4.4"
-        />
-        <CardItem 
-          CardTitle="Defekt høytaler"
-          CardDescription="Skadet elementer"
-          CardRating="★3.4"
-        />
-        <CardItem 
-          CardTitle="Ødelagt vaskemaskin"
-          CardRating="2.6"
-        />
+         ))}
     </View>
   );
 }
